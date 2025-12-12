0a14577307106c67a5bf17879f8c03cdabc5e347
Mateus Eduardo Gobo <mateus.gobo@gmail.com>
2025-12-12 17:36:03 -0300

    Ativando selecao de arquivos nos testes automatizados

--- validacaoProcessosOficio/src/test/java/br/com/just/peticao/inicial/test/ValidarProcessosTest.java
+++ validacaoProcessosOficio/src/test/java/br/com/just/peticao/inicial/test/ValidarProcessosTest.java
@@ -1,43 +1,43 @@
 package br.com.just.peticao.inicial.test;
 
-import br.com.jus.peticao.inicial.impl.SleepActionImpl;
 import br.com.pje.model.TokenPattern;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.net.ssl.SSLContext;
-import java.io.*;
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Logger;
 
 import static br.com.jus.peticao.inicial.impl.DeserializeToObject.apply;
-import static br.com.jus.peticao.inicial.impl.PeticaoSupplier.*;
+import static br.com.jus.peticao.inicial.impl.PeticaoSupplier.url;
 import static br.com.jus.peticao.inicial.impl.SSLContextToRequest.instanceOf;
 import static br.com.jus.peticao.inicial.impl.SleepActionImpl.*;
-import static br.com.jus.peticao.inicial.impl.SleepActionImpl.sleep;
 
 public class ValidarProcessosTest extends BaseIntegrationTest {
 
     @BeforeEach
     public void setUp() {
         driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<>();
         wait = new WebDriverWait(driver, Duration.ofSeconds(2));
@@ -50,10 +50,10 @@ public class ValidarProcessosTest extends BaseIntegrationTest {
 
     private void lerProcessos() {
         File file = path.toFile();
-        if(file.exists()) {
-            try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+        if (file.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 br.lines().forEach(line -> System.out.println(line));
-            }catch (IOException e) {
+            } catch (IOException e) {
                 Logger.getLogger(ValidarProcessosTest.class.getSimpleName()).severe("Erro na leitura do arquivo de processos");
             }
         }
@@ -75,13 +75,14 @@ public class ValidarProcessosTest extends BaseIntegrationTest {
     }
 
     @Test
-    public void protocolarPeticaoInicial() throws InterruptedException {
+    public void protocolarPeticaoInicial() throws InterruptedException, AWTException {
         driver.get(url.get());
-        sleep.actionComponent(driver, "//input[@id='username']",  false,"324.547.258-78");
-        sleep.actionComponent(driver, "//input[@id='password']", false,"Megatirador65#");
+        sleep.actionComponent(driver, "//input[@id='username']", false, "324.547.258-78");
+        sleep.actionComponent(driver, "//input[@id='password']", false, "Megatirador65#");
         click.apply(driver, "//input[@id='kc-login']");
 
         definirProcessosPeticaoIntercorrentes();
+        selecionarArquivosPeticao();
     }
 
     private void definirProcessosPeticaoIntercorrentes() throws InterruptedException {
@@ -97,12 +98,33 @@ public class ValidarProcessosTest extends BaseIntegrationTest {
         threadSleep_2000_ms.get();
         pularButtonAction.apply(driver);
 
-        sleep.actionComponent(driver, "mat-input-1",  true,"0800102-27.2019.8.19.0031");
+        sleep.actionComponent(driver, "mat-input-1", true, "0800102-27.2019.8.19.0031");
         click.apply(driver, "//button[@class='mat-focus-indicator ml-3 mat-raised-button mat-button-base mat-primary']");
         threadSleep_ms.apply(10000);
 
         click.apply(driver, "//button[@id='botao-acao']");
         threadSleep_ms.apply(10000);
+    }
+
+    private void selecionarArquivosPeticao() throws AWTException {
+        click.apply(driver, "(//button[@type='button'])[2]");
+
+        // File path to upload
+        String filePath = "C:\\Users\\mateu.gobo\\JUS-BR\\PETICOES\\PETICAO_INICIAL_A.pdf";
+
+        // Copy to clipboard
+        StringSelection s = new StringSelection(filePath);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(s, null);
+
+        // Paste and press Enter
+        Robot robot = new Robot();
+        robot.delay(1000);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
     }
 
     @Test
